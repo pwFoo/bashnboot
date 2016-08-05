4b06743e9d23fb568d9445a678285f16968138e3
Tyler Samples <samples.tm@gmail.com>
2016-08-04 22:30:25 -0600

    Documentation and initial work on env.

--- bashnboot.sh
+++ bashnboot.sh
@@ -1,28 +1,46 @@
 #!/bin/sh
 
-BUSYBOX="1.25.0"
-KERNEL="4.6.5"
-JOBS="8"
+set -o allexport
+test -f ./.env && source ./.env
+
+BUSYBOX_VER=${BUSYBOX:-"1.25.0"}
+KERNEL_VER=${KERNEL:-"4.6.5"}
+JOBS_NUM=${JOBS:-"8"}
+ISO_NAME=${NAME:-"bashnboot"}
 
 # Creates a clean directory to download/extract and compile busybox.
 staging() {
   mkdir staging && cd "$_"
 
-  curl -s https://busybox.net/downloads/busybox-${BUSYBOX}.tar.bz2 | tar -jxf -
-  cd busybox-${BUSYBOX}/
+  echo "Downloading busybox ${BUSYBOX_VER}."
+
+  # Download and extract busybox.
+  curl -s https://busybox.net/downloads/busybox-${BUSYBOX_VER}.tar.bz2 | tar -jxf -
+  cd busybox-${BUSYBOX_VER}/
+
+  echo "Compiling busybox ${BUSYBOX_VER}."
 
   make distclean defconfig 2>&1 > /dev/null
+
+  # Set busybox to be statically compiled so it can run on it's own.
   sed -i "s/.*CONFIG_STATIC.*/CONFIG_STATIC=y/" .config
-  make -j${JOBS} busybox install 2>&1 > /dev/null
+  make -j${JOBS_NUM} busybox install 2>&1 > /dev/null
   cd ../..
 }
 
 # Creates the base structure for the file system and creates a simple init.
 initrd() {
   mkdir initrd && cd "$_"
-  mkdir -p {bin,sbin,etc,proc,sys,usr/{bin,sbin}}
-  cp -a ../staging/busybox-${BUSYBOX}/_install/* .
 
+  # Create the base filesystem.
+  mkdir -p {bin,sbin,etc,proc,sys,usr/{bin,sbin}}
+
+  # Copy all of our executables from busybox into their respective directories.
+  cp -a ../staging/busybox-${BUSYBOX_VER}/_install/* .
+
+  echo "Creating init script."
+
+  # Nothing special in the init file, simply mount proc and sysfs and then exec sh.
   cat > init << EOL
 #!/bin/sh
 
@@ -32,19 +50,32 @@ mount -t sysfs none /sys
 exec /bin/sh
 EOL
 
+  # Ensure our init is executable.
   chmod +x init
+
+  echo "Generating initrd."
+
+  # Wrap up our filesystem, format it as cpio and then gzip it.
   find . -print0 | cpio --null -o --format=newc | gzip -9 > ../initrd.gz
+
   cd ..
 }
 
 # Compile our kernel and generate our iso.
 kernel() {
   mkdir kernel && cd "$_"
-  curl -sL http://kernel.org/pub/linux/kernel/v4.x/linux-${KERNEL}.tar.xz | tar -Jxf -
-  cd linux-${KERNEL}
-  make -j${JOBS} ARCH=x86_64 mrproper defconfig bzImage 2>&1 > /dev/null
-  make -j${JOBS} isoimage FDINITRD=../../initrd.gz 2>&1 > /dev/null
-  cp arch/x86/boot/image.iso ../../bashnboot.iso
+
+  echo "Downloading Linux ${KERNEL_VER}."
+
+  curl -sL http://kernel.org/pub/linux/kernel/v4.x/linux-${KERNEL_VER}.tar.xz | tar -Jxf -
+  cd linux-${KERNEL_VER}
+
+  echo "Compiling Linux ${KERNEL_VER}."
+
+  make -j${JOBS_NUM} ARCH=x86_64 mrproper defconfig bzImage 2>&1 > /dev/null
+  make -j${JOBS_NUM} isoimage FDINITRD=../../initrd.gz 2>&1 > /dev/null
+
+  cp arch/x86/boot/image.iso ../../${ISO_NAME}.iso
   cd ../..
 }
 
--- readme.md
+++ readme.md
@@ -6,3 +6,26 @@ $ cd build
 $ ../bashnboot.sh
 $ qemu-system-x86_64 -cdrom bashnboot.iso
 ```
+
+### Optionally
+From within your build directory you can create a .env file such as:
+```
+cat > .env << EOL
+# Change the busybox version downloaded and compiled.
+BUSYBOX="1.25.0"
+
+# Change the Linux kernel version downloaded and compiled.
+KERNEL="4.6.5"
+
+# Number of compile jobs used when compiling the above.
+JOBS="9"
+
+# Change the resulting iso name (${NAME}.iso)
+NAME="bashnboot"
+EOL
+```
+## TODO
+- [ ] Separate sources from compiling directories.
+- [ ] Support rebuilting.
+- [ ] More documentation.
+- [ ] Add more options for configuring, cpu arch? init override?
